cf8272a051f8267b405cd54ea7bdfcb9dd769943
Дмитрий Алехин <bevstrogan@gmail.com>
2023-11-08 23:55:14 +0300

    new function and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def higher_register(text):
+    "Функция переводит текст в верхний регистр"
     return str(text.upper())
+
+def First_hight_letter(text):
+    "Функция делает первую букву у каждого слова большой"
+    return str(text.title())
